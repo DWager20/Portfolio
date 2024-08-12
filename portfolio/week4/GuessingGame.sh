208c2c336433a501e302d877440122261d4315b9
DWager20 <davidwager2022@gmail.com>
2024-08-12 11:12:39 +0000

    Modified guessinggame.sh to handle non integer input and added colours

--- portfolio/week4/GuessingGame.sh
+++ portfolio/week4/GuessingGame.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ----------------------------------------------------
+# ---------------------------------------------------------------
 # Title: Guessing Game
 #
 # Description:
@@ -12,17 +12,16 @@
 #
 # Author: David Wager
 # Date created: 30 July 2024
-# Last Edited: 30 July 2024 
-# ----------------------------------------------------
+# Last Edited: 12 August 2024 added check for specical characters 
+#               immproved formatting and added colours.
+# ----------------------------------------------------------------
       
- #This function prints a given error with a red ERROR: at the start
+#This function prints a given error with a red ERROR: at the start
       
 printError() 
       
-{ 
-      
-    echo -e "\033[31mERROR:\033[0m $1" 
-      
+{   
+    echo -e "\033[31mERROR:\033[0m $1"     
 } 
       
  
@@ -33,11 +32,22 @@ getNumber()
       
 { 
       
-    read -p "$1: " 
+    read -p "$1: "
+
+    # Handles non integer inputs
+
+    until [[ $REPLY =~ ^[0-9]+$ ]]; do
+        
+        printError "\033[32mInput must be an integer between $2 and $3\n"
+
+        read -p "$1: "
+    done
       
+    # Handles out of bounds inputs
+
     while (( $REPLY < $2 || $REPLY> $3 )); do 
       
-        printError "Input must be between $2 and $3" 
+        printError "\033[32mInput must be between $2 and $3\n" 
       
         read -p "$1: " 
       
@@ -45,12 +55,12 @@ getNumber()
       
 } 
 
-# Explicitly state the correct value so can be easily change and set the counter
+# Explicitly state the correct value so can be easily changed and set the number of tries counter
 
 correct=42
 counter=1
-      
-echo "Welcome to the guesing game!" 
+
+echo -e "\n\033[032mWelcome to the guesing game!" 
 echo ""
       
 # Gets number and makes sure it is within range
@@ -62,9 +72,9 @@ getNumber "Please type a number between 1 and 100" 1 100
 until [ $REPLY -eq $correct ]; do
     ((counter++))
     if [ $REPLY -lt $correct ]; then
-        echo "Too low! Try again."
+        echo -e "\n\033[031mToo low!\033[032m Try again.\n"
     elif [  $REPLY -gt $correct ]; then
-        echo "Too high! Try again."
+        echo -e "\n\033[031mToo high!\033[032m Try again.\n"
     fi 
 
     getNumber "Please type a number between 1 and 100" 1 100
@@ -73,14 +83,14 @@ done
 
 # Congratulatory message on completion
 
-echo "Congratulations you guessed it! $correct is the correct number"
+echo -e "\nCongratulations you guessed it! \033[1;033m$correct\033[0;032m is the correct number"
 
 # Informing user of the number of tries
 
 if [ $counter -eq 1 ]; then
-    echo "You got it first time!"
+    echo -e "You got it first attempt!\n"
 else
-    echo "It took you $counter tries!"
+    echo -e "It took you $counter valid tries!\n"
 fi
   
 
